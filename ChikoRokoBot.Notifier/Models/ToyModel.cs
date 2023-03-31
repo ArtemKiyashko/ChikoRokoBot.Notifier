4d87b183a8e143e1c444adafe3e84a12633ce54b
Artem Kiyashko <artem86@hotmail.co.uk>
2023-03-31 14:58:32 +0200

    init

---
+++ ChikoRokoBot.Notifier/Models/ToyModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace ChikoRokoBot.Notifier.Models
+{
+    public record ToyModel(
+        [property: JsonPropertyName("toyId")] int? ToyId,
+        [property: JsonPropertyName("archiveName")] string ArchiveName
+    );
+}
+
---
+++ ChikoRokoBot.Notifier/Notify.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace ChikoRokoBot.Notifier
+{
+    public class Notify
+    {
+        [FunctionName("Notify")]
+        public void Run([QueueTrigger("notifydrops", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
+
