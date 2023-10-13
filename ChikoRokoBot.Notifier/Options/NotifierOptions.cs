97a152e15a014adffe424551a2fae003458eb2f5
Artem Kiyashko <artem86@hotmail.co.uk>
2023-10-13 17:33:41 +0200

    remove redundant options

--- ChikoRokoBot.Notifier/Options/NotifierOptions.cs
+++ ChikoRokoBot.Notifier/Options/NotifierOptions.cs
@@ -4,8 +4,6 @@ namespace ChikoRokoBot.Notifier.Options
 	public class NotifierOptions
 	{
 		public string TelegramBotToken { get; set; }
-		public string StorageAccount { get; set; } = "UseDevelopmentStorage=true";
-		public string UsersTableName { get; set; } = "users";
 	}
 }
 
